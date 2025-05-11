8f2770025643c0a1e972cb2432652eec1e105898
Enlas <ceh951230@icloud.com>
2025-05-11 11:22:05 +0800

    左上角改了一下

--- pages/tracking.php
+++ pages/tracking.php
@@ -27,7 +27,10 @@ if (!isset($_SESSION['user_id'])) {
   </div>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <a class="navbar-brand" href="tracking.php">GreenGo</a>
+    <h1 class="mb-4 text-success-emphasis fw-bold">
+        <i class="bi bi-leaf" style="color: #90ee90;">GreenGo </i>
+        <i class="bi bi-leaf" style="color:rgb(200, 255, 200);font-size:19px;">讓環保充斥在你我生活之間</i>
+      </h1>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -42,12 +45,6 @@ if (!isset($_SESSION['user_id'])) {
     </div>
   </nav>
   <div class="container py-5">
-    <div>
-      <h1 class="mb-4 text-success-emphasis fw-bold">
-        <i class="bi bi-leaf" style="color: #90ee90;">GreenGo </i>
-        <i class="bi bi-leaf" style="color:rgb(200, 255, 200);font-size:19px;">讓環保充斥在你我生活之間</i>
-      </h1>
-    </div>
     <div id="tracking-section" class="card mb-4" style="border-radius: 15px;">
       <div class="card-body" style="border-radius: 15px;">
         <div class="mb-3">
