deb3d0f0bae1cbacfe6d9f918317770e695f8a4b
Enlas <ceh951230@icloud.com>
2025-06-15 16:09:24 +0800

    充滿內容的資料庫

--- SQL/carbon_tracker.sql
+++ SQL/carbon_tracker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2025-06-15 09:09:57
+-- 產生時間： 2025-06-15 10:08:39
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -52,7 +52,25 @@ CREATE TABLE `friends` (
   `status` enum('pending','accepted','rejected') DEFAULT 'pending' COMMENT '好友狀態：待接受、已接受、已拒絕',
   `created_at` datetime NOT NULL DEFAULT current_timestamp() COMMENT '建立時間',
   `initiator_id` int(11) NOT NULL COMMENT '發起請求的用戶 ID'
-) ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `friends`
+--
+
+INSERT INTO `friends` (`id`, `user_id`, `friend_id`, `status`, `created_at`, `initiator_id`) VALUES
+(1, 1, 10, 'accepted', '2025-06-15 15:21:53', 1),
+(2, 1, 2, 'accepted', '2025-06-15 15:24:32', 1),
+(3, 2, 9, 'accepted', '2025-06-15 15:27:12', 9),
+(4, 2, 3, 'accepted', '2025-06-15 15:28:23', 2),
+(5, 7, 8, 'accepted', '2025-06-15 15:32:36', 8),
+(7, 2, 7, 'accepted', '2025-06-15 15:35:26', 2),
+(8, 3, 4, 'accepted', '2025-06-15 15:57:51', 3),
+(9, 1, 5, 'rejected', '2025-06-15 16:01:36', 5),
+(10, 1, 7, 'rejected', '2025-06-15 16:02:05', 1),
+(11, 6, 8, 'accepted', '2025-06-15 16:03:12', 6),
+(12, 7, 10, 'pending', '2025-06-15 16:04:59', 7),
+(13, 1, 8, 'rejected', '2025-06-15 16:07:24', 1);
 
 -- --------------------------------------------------------
 
@@ -79,17 +97,16 @@ CREATE TABLE `personal_page` (
 --
 
 INSERT INTO `personal_page` (`user_id`, `username`, `bio`, `country_code`, `city`, `gender`, `birthdate`, `activity_level`, `created_at`, `last_update`) VALUES
-(1, 'aaa', NULL, NULL, NULL, NULL, NULL, NULL, '2025-06-09 01:31:02', NULL),
-(2, 'bbb', NULL, NULL, NULL, NULL, NULL, NULL, '2025-06-09 01:31:03', NULL),
-(3, 'ccc', NULL, NULL, NULL, NULL, NULL, NULL, '2025-06-09 01:31:04', NULL),
-(4, 'ddd', NULL, NULL, NULL, NULL, NULL, NULL, '2025-06-09 01:31:05', NULL),
-(5, 'eee', NULL, NULL, NULL, NULL, NULL, NULL, '2025-06-09 01:31:06', NULL),
-(6, 'fff', NULL, NULL, NULL, NULL, NULL, NULL, '2025-06-09 01:31:07', NULL),
-(7, 'ggg', NULL, NULL, NULL, NULL, NULL, NULL, '2025-06-09 01:31:08', NULL),
-(8, 'hhh', NULL, NULL, NULL, NULL, NULL, NULL, '2025-06-09 01:31:09', NULL),
-(9, 'iii', NULL, NULL, NULL, NULL, NULL, NULL, '2025-06-09 01:31:10', NULL),
-(10, 'jjj', NULL, NULL, NULL, NULL, NULL, NULL, '2025-06-09 01:31:11', NULL),
-(11, '123', NULL, NULL, NULL, NULL, NULL, NULL, '2025-06-15 00:49:11', NULL);
+(1, 'aaa', '我是北七', '日本', '東京', '女', '2025-06-03', '中', '2025-06-09 01:31:02', '2025-06-15 15:54:57'),
+(2, 'bbb', '烏拉呀哈', '台灣', '其他', '女', '1989-06-04', '低', '2025-06-09 01:31:03', '2025-06-15 15:26:05'),
+(3, 'ccc', '我不知道寫什麼', '台灣', '其他', '女', '2019-07-15', '低', '2025-06-09 01:31:04', '2025-06-15 15:57:39'),
+(4, 'ddd', '猜猜我是誰', '台灣', '台北', '男', '2000-02-11', '', '2025-06-09 01:31:05', '2025-06-15 15:59:30'),
+(5, 'eee', '我是小丑', '台灣', '其他', '其他', '2025-06-27', '低', '2025-06-09 01:31:06', '2025-06-15 16:01:09'),
+(6, 'fff', '人家剛滿18歲', '日本', '東京', '男', '2009-11-20', '低', '2025-06-09 01:31:07', '2025-06-15 16:02:56'),
+(7, 'ggg', '天線寶寶~天線寶寶~說！你！好！', '美國', '紐約', '男', '2040-11-21', '高', '2025-06-09 01:31:08', '2025-06-15 16:04:35'),
+(8, 'hhh', '我要環遊世界!!!', '美國', '其他', '其他', '2025-06-15', '高', '2025-06-09 01:31:09', '2025-06-15 15:32:26'),
+(9, 'iii', '我是誰我在哪', '美國', '紐約', '男', '0000-00-00', '', '2025-06-09 01:31:10', '2025-06-15 15:27:02'),
+(10, 'jjj', '赫赫哈ㄏ一ˋ', '', '', '', '0000-00-00', '', '2025-06-09 01:31:11', '2025-06-15 15:19:38');
 
 -- --------------------------------------------------------
 
@@ -106,6 +123,19 @@ CREATE TABLE `redeem_history` (
   `points_used` int(11) NOT NULL,
   `redeem_time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `redeem_history`
+--
+
+INSERT INTO `redeem_history` (`id`, `user_id`, `reward_id`, `reward_name`, `points_used`, `redeem_time`) VALUES
+(1, 2, 2, '腳踏車租借券', 100, '2025-06-15 15:24:35'),
+(2, 2, 1, '環保袋', 50, '2025-06-15 15:24:40'),
+(3, 8, 2, '腳踏車租借券', 100, '2025-06-15 15:31:24'),
+(4, 8, 17, '可持續生活工作坊', 350, '2025-06-15 15:31:30'),
+(5, 1, 2, '腳踏車租借券', 100, '2025-06-15 15:55:44'),
+(6, 5, 9, '可持續時尚折扣券', 90, '2025-06-15 16:00:45'),
+(7, 7, 2, '腳踏車租借券', 100, '2025-06-15 16:05:39');
 
 -- --------------------------------------------------------
 
@@ -169,6 +199,32 @@ CREATE TABLE `travel_records` (
   `record_time` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- 傾印資料表的資料 `travel_records`
+--
+
+INSERT INTO `travel_records` (`id`, `user_id`, `transport`, `distance`, `footprint`, `points`, `record_time`) VALUES
+(1, 2, '大眾運輸', 79.667, 6.21403, 15, '2025-06-15 15:24:20'),
+(2, 2, '步行', 105.487, 0, 210, '2025-06-15 15:24:25'),
+(3, 9, '腳踏車', 67.026, 0, 67, '2025-06-15 15:26:27'),
+(4, 3, '步行', 34.763, 0, 69, '2025-06-15 15:28:36'),
+(5, 8, '腳踏車', 278.137, 0, 278, '2025-06-15 15:30:26'),
+(6, 8, '汽車', 114.811, 11.9403, 0, '2025-06-15 15:30:45'),
+(7, 8, '大眾運輸', 913.118, 71.2232, 182, '2025-06-15 15:31:06'),
+(8, 7, '腳踏車', 84.016, 0, 84, '2025-06-15 15:32:56'),
+(9, 7, '機車', 316.716, 25.0206, 0, '2025-06-15 15:33:09'),
+(10, 1, '腳踏車', 116.515, 0, 116, '2025-06-15 15:55:14'),
+(11, 1, '汽車', 290.874, 30.2509, 0, '2025-06-15 15:55:19'),
+(12, 1, '步行', 20.563, 0, 41, '2025-06-15 15:55:23'),
+(13, 3, '汽車', 11.64, 1.21056, 0, '2025-06-15 15:56:17'),
+(14, 4, '大眾運輸', 44.594, 3.47833, 8, '2025-06-15 15:58:06'),
+(15, 4, '腳踏車', 22.844, 0, 22, '2025-06-15 15:58:11'),
+(16, 5, '腳踏車', 81.482, 0, 81, '2025-06-15 16:00:03'),
+(17, 5, '機車', 4.339, 0.342781, 0, '2025-06-15 16:00:12'),
+(18, 5, '汽車', 57.466, 5.97646, 0, '2025-06-15 16:00:29'),
+(19, 5, '腳踏車', 25.758, 0, 25, '2025-06-15 16:00:33'),
+(20, 7, '腳踏車', 52.091, 0, 52, '2025-06-15 16:03:26');
+
 -- --------------------------------------------------------
 
 --
@@ -196,8 +252,7 @@ INSERT INTO `users` (`user_id`, `username`, `password`) VALUES
 (7, 'ggg', '$2y$10$Ri0qfQjiidUYdLiOFzfV9.aYtuz4Zj.uzTKjsl/U7Resj0rltUu5i'),
 (8, 'hhh', '$2y$10$Ri0qfQjiidUYdLiOFzfV9.aYtuz4Zj.uzTKjsl/U7Resj0rltUu5i'),
 (9, 'iii', '$2y$10$Ri0qfQjiidUYdLiOFzfV9.aYtuz4Zj.uzTKjsl/U7Resj0rltUu5i'),
-(10, 'jjj', '$2y$10$Ri0qfQjiidUYdLiOFzfV9.aYtuz4Zj.uzTKjsl/U7Resj0rltUu5i'),
-(11, '123', '$2y$10$Z5mh3wcm0SrIFKx4r3z9KeMfA1peZdXKboKR6nuIxiP1s6GnVo2Ly');
+(10, 'jjj', '$2y$10$Ri0qfQjiidUYdLiOFzfV9.aYtuz4Zj.uzTKjsl/U7Resj0rltUu5i');
 
 --
 -- 已傾印資料表的索引
@@ -261,7 +316,7 @@ ALTER TABLE `users`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `friends`
 --
 ALTER TABLE `friends`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `personal_page`
@@ -273,13 +328,13 @@ ALTER TABLE `personal_page`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `redeem_history`
 --
 ALTER TABLE `redeem_history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `travel_records`
 --
 ALTER TABLE `travel_records`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `users`
